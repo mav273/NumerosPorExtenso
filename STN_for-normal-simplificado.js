43993c8a90a9efae18f4fc9d0b3dea5ca5984b94
mav273 <marcusvslourenco@fac.pe.senac.br>
2022-06-06 12:17:58 -0300

    Casos com "," ao invés de "e" adicionados, decimais funcionando

--- STN_for-normal-simplificado.js
+++ STN_for-normal-simplificado.js
@@ -1,50 +1,40 @@
 //Tentando deixar o codigo mais bonito
 
-
 //Import do objeto com os números
 const {por_extenso, maiorque999,decimal,negativos} = require('./num_objeto')
 const chaves  = Object.keys(por_extenso)
 
 function verificarDecimal(se){
-    const arr = se.split(' ')
-    const dec = arr.some(r => decimal.includes(r))
+    const dec = se.some(r => decimal.includes(r))
     if (dec){
         const verdadeiro = []
         for (valor of decimal){
-            const tem = arr.includes(valor)
+            const tem = se.includes(valor)
             if (tem) verdadeiro.push(valor)
         }
-        if (verdadeiro.length > 1) throw 'Só é permitido somente uma parte decimal'
+        if (verdadeiro.length > 1) throw 'É permitido somente uma parte decimal'
         return verdadeiro
     }
     else return null
 }
 
 function string2number(string){
-    //String sendo "ajeitada"
+    //String sendo "organizada"
     const onespace   = string.replace(/  +/g, ' ')
-    const trim       = onespace.trim()
+    const onlyE      = onespace.replace(/, /g, ' e ')
+    const trim       = onlyE.trim()
     const lower      = trim.toLowerCase()
-    const hasdecimal = verificarDecimal(lower)
-
+    const tudo       = lower.split(' ')
+    const hasdecimal = verificarDecimal(tudo)
     //verifica se a string possui numeros decimais
-    let array
+    let array  = tudo
     let array2
+    if (hasdecimal != null) { 
+        const index  = tudo.indexOf(hasdecimal[0])
+        array2 = tudo.slice(index, tudo.length)
+    }
     let stResult
     let ndResult
-    let isdecimal = false
-    let depois
-    if (hasdecimal  != null){
-        const ponto  = hasdecimal[0]
-        const array1 = lower.split(ponto) 
-        const antes  = array1[0]
-        if (ponto != 'reais') {
-            depois = array1[1]
-            array2    = depois.split(' ')
-        }
-        array    = antes.split(' ')
-    }
-    else array = lower.split(' ')
     //
 
     let resultado    = 0 
@@ -53,30 +43,30 @@ function string2number(string){
 
 
     let total = 0
-    for (var i = 0;i < array.length;i++){
+    for (var i = 0;i < tudo.length;i++){
         //seleciona o valor que está sendo analisado no array
-        const atual  = array[i]
-
+        const atual  = tudo[i]
         //Caso o valor seja o separador do decimal, reseta os valores e guarda o resultado anterior ao decimal
-        if (decimal.includes(atual)) isdecimal  = true 
-        if (decimal == true){
+        if (decimal.includes(atual)){
             stResult   = resultado
+            resultado  = 0
             armazenado = 0
-            anterior   = 0
+            anterior   = null
             array      = array2
         }
 
-        //checa se o valor existe no por_extenso
+
+        //verifica se o valor existe no por_extenso
         const possui = chaves.includes(atual)
-        //pega o valor através do nome por extenso
+        //verifica o valor através do nome por extenso
         const num = por_extenso[atual];
-        //checa se antes do valor atual, houve alguma interação com os valores maiores que mil
+        //verifica se antes do valor atual, houve alguma interação com os valores maiores que mil
         const previous   = array.slice(0,i).some(r=> maiorque999.includes(r))
         const next       = array.slice(i + 1,array.length).some(r=> maiorque999.includes(r))
         const morett     = maiorque999.includes(atual)
 
-        //substituição do valor anterior com "e"
-        if (atual == 'e') anterior = atual        
+        //substituição do valor anterior com "e" ou ","
+        if (atual == 'e') anterior = atual 
         if (possui){
             if(anterior == 'e' || resultado == 0){
                 if (next){
@@ -98,20 +88,18 @@ function string2number(string){
                     armazenado = armazenado + num
                 }
                 if (morett && armazenado > 0){
-                    console.log(resultado,armazenado,anterior)
                     resultado  = resultado - armazenado
                     armazenado = armazenado * num 
                     resultado  = resultado + armazenado
                     armazenado = 0 
                 }
-
             }
         anterior = num
         }
     }
 
 
-    if (isdecimal == true){
+    if (hasdecimal != null){
         ndResult = resultado
         total = ` ${stResult}.${ndResult}`
         total = parseFloat(total)
@@ -119,13 +107,14 @@ function string2number(string){
     else {
         total = resultado
     }
-    
-    if(lower.split(' ').includes('reais')) total = 'R$ '+ parseFloat(total).toFixed(2)
+    if(!(tudo.includes('reais')) && tudo.includes('centavos')){ 
+        total = total/100
+        total =  'R$ ' + parseFloat(total).toFixed(2)
+    }
+    if(tudo.includes('reais')) total = 'R$ '+ parseFloat(total).toFixed(2)
 
-    if (lower.split(' ').some(r => negativos.includes(r))) total = total * -1
+    if (tudo.some(r => negativos.includes(r))) total = total * -1
     console.log(total)
 }
     
-
-const string = process.argv[2];
-string2number(string);
+module.exports = { string2number }
