d0fdfec555c98e653e784ed9570a843129961eb7
mav273 <marcusvslourenco@fac.pe.senac.br>
2022-06-06 12:16:05 -0300

    decimos e centesimos adicionados ao por_extenso

--- num_objeto.js
+++ num_objeto.js
@@ -10,13 +10,22 @@ zero: 0,
 um: 1,
 dois: 2,
 três: 3,
+tres: 3,
 quatro: 4,
 cinco: 5,
 seis: 6,
+meia: 6,
+meio: 6,
 sete: 7,
 oito: 8,
 nove: 9,
+
 dez: 10,
+decimos: 10,
+décimos: 10,
+décimo:10,
+decimo:10,
+
 onze: 11,
 doze: 12,
 treze: 13,
@@ -35,8 +44,15 @@ sessenta: 60,
 setenta: 70,
 oitenta: 80,
 noventa: 90,
+
 cem: 100,
 cento:100,
+centos:100,
+centésimos: 100,
+centesimos: 100,
+centésimo: 100,
+centesimo: 100,
+
 duzentos: 200,
 trezentos:300,
 quatrocentos: 400,
@@ -45,6 +61,7 @@ seiscentos: 600,
 setecentos: 700,
 oitocentos: 800,
 novecentos: 900,
+
 mil: 1000,
 
 milhão: 1000000,
@@ -55,7 +72,7 @@ milhoes: 1000000,
 bilhão: 1000000000,
 bilhao: 1000000000,
 bilhões: 1000000000,
-bilhoes: 1000000000,
+bilhoes: 1000000000
 
 };
 
@@ -63,7 +80,10 @@ const maiorque999 = [
     'mil', 'milhão','milhao','milhões','milhoes','bilhão','bilhao','bilhões','bilhoes'
 ]
 
-const decimal = ['virgula','ponto','vírgula','.',',','reais']
+const decimal = ['virgula','ponto','vírgula','.','reais','inteiros','inteiro']
 
 const negativos = ['menos','negativo','negativos']
+
+const soma = [',','e']
+
 module.exports = {por_extenso, maiorque999,decimal,negativos}
